a8baa6e3357f96e333c19ac10569afe559339332
labuser <bikash.agrawala@in.ey.com>
2025-04-09 04:47:41 +0000

    Commit initial code

---
+++ src/test/java/com/ey/banking/viewTRansaction/ViewTRansactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ey.banking.viewTRansaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewTRansactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
