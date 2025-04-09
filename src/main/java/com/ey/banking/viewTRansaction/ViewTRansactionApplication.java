da2a63cdaf009ecb84c9a11eeaf34f736ce6d02f
labuser <bikash.agrawala@in.ey.com>
2025-04-09 04:47:41 +0000

    Commit initial code

---
+++ src/main/java/com/ey/banking/viewTRansaction/ViewTRansactionApplication.java
@@ -0,0 +1,13 @@
+package com.ey.banking.viewTRansaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewTRansactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewTRansactionApplication.class, args);
+	}
+
+}
